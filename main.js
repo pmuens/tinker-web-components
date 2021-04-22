95df7029fdcba591b41bb0c6980dc3fd683a246d
Philipp Muens <raute1337@gmx.de>
2021-04-22 13:17:11 +0200

    Implement attribute functionality

--- main.js
+++ main.js
@@ -40,6 +40,21 @@ class Button extends HTMLElement {
 
     this._shadowRoot = this.attachShadow({ mode: "open" });
     this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.$button = this._shadowRoot.querySelector("button");
+  }
+
+  static get observedAttributes() {
+    return ["label"];
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    this[name] = newVal;
+    this.render();
+  }
+
+  render() {
+    this.$button.innerHTML = this.label;
   }
 }
 
